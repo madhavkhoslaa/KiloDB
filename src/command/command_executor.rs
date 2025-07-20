607580fc420057a10925e048a5ad193bb528b32c
Madhav Khosla <28945483+madhavkhoslaa@users.noreply.github.com>
2025-07-20 15:42:39 +0530

    Revert "added function impl need to review"
    This reverts commit c0fd79f2f79926ef02fc2822831c3f29c975f292.

--- src/command/command_executor.rs
+++ src/command/command_executor.rs
@@ -1,6 +1,4 @@
-use crate::command::executor::{
-    del, dbsize, echo, exists, expire, flushdb, get, hdel, hexists, hget, hgetall, hset, lpop, lpush, lrange, ping, rpop, rpush, sadd, set, sismember, smembers, srem, zadd, zrange, zrem
-};
+use crate::command::executor::{del, dbsize, echo, exists, expire, flushdb, get, ping, set};
 use crate::traits::command::commandExecutor;
 use crate::{command::command_enum::Command, store_containers::core_context::context};
 use std::error::Error;
@@ -28,32 +26,6 @@ impl command_executor {
             Command::DBSIZE => dbsize::dbsize::execute(command, context),
             Command::ECHO { message: _message } => echo::echo::execute(command, context),
             Command::PING => ping::ping::execute(command, context),
-            
-            // Hash commands
-            Command::HSET { key: _key, fields: _fields } => hset::hset::execute(command, context),
-            Command::HGET { key: _key, field: _field } => hget::hget::execute(command, context),
-            Command::HGETALL { key: _key } => hgetall::hgetall::execute(command, context),
-            Command::HDEL { key: _key, fields: _fields } => hdel::hdel::execute(command, context),
-            Command::HEXISTS { key: _key, field: _field } => hexists::hexists::execute(command, context),
-            
-            // List commands
-            Command::LPUSH { key: _key, values: _values } => lpush::lpush::execute(command, context),
-            Command::RPUSH { key: _key, values: _values } => rpush::rpush::execute(command, context),
-            Command::LPOP { key: _key } => lpop::lpop::execute(command, context),
-            Command::RPOP { key: _key } => rpop::rpop::execute(command, context),
-            Command::LRANGE { key: _key, start: _start, stop: _stop } => lrange::lrange::execute(command, context),
-            
-            // Set commands
-            Command::SADD { key: _key, members: _members } => sadd::sadd::execute(command, context),
-            Command::SREM { key: _key, members: _members } => srem::srem::execute(command, context),
-            Command::SMEMBERS { key: _key } => smembers::smembers::execute(command, context),
-            Command::SISMEMBER { key: _key, member: _member } => sismember::sismember::execute(command, context),
-            
-            // Sorted set commands
-            Command::ZADD { key: _key, entries: _entries } => zadd::zadd::execute(command, context),
-            Command::ZREM { key: _key, members: _members } => zrem::zrem::execute(command, context),
-            Command::ZRANGE { key: _key, start: _start, stop: _stop } => zrange::zrange::execute(command, context),
-            
             _ => Ok(b"$-1\r\n".to_vec()),
         }
     }
--- src/store/mod.rs
+++ src/store/mod.rs
@@ -1,6 +1,2 @@
 pub mod string_store;
 pub mod vector_store;
-pub mod hash_store;
-pub mod list_store;
-pub mod set_store;
-pub mod sorted_set_store;
